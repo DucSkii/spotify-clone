d1d82c89900817afb1036ce553fddf3758e54f6a
DucSkii <ducvietdao@live.co.uk>
2021-03-16 13:11:18 +0000

    implemented sticky header with transparent background on pageYOffset

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AccountButton from '../AccountButton'
 import { useSelector } from 'react-redux'
 
@@ -7,9 +7,19 @@ import './index.css'
 const Header = () => {
 
   const open = useSelector(state => state.dropdown.open)
+  const [style, setStyle] = useState({ opacity: '0' })
+
+  window.onscroll = () => {
+    if (window.pageYOffset >= 100) {
+      setStyle({ opacity: '1' })
+    } else {
+      setStyle({ opacity: '0' })
+    }
+  }
 
   return (
     <div className='header'>
+      <div className='header-colour' style={style} />
       <div className={open ? 'header-accountButton-open' : 'header-accountButton'} >
         <AccountButton />
       </div>
