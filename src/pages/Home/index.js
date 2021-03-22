77d729ff3bca5adf37f888d7b0442d2000990930
DucSkii <ducvietdao@live.co.uk>
2021-03-22 14:23:55 +0000

    added playlist display to home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import BackgroundGradient from '../../components/BackgroundGradient'
+import PlaylistDisplay from '../../components/PlaylistDisplay'
 import { setBackgroundGradient } from '../../redux/ducks/generalReducer'
 import './index.css'
 
@@ -14,11 +15,28 @@ const Home = () => {
     dispatch(setBackgroundGradient('rgb(69,23,3)'))
   }, [dispatch])
 
+  const renderPlaylists = () => {
+    return playlists?.items?.map((playlist, index) => {
+      return (
+        <div key={index} style={{ marginRight: '25px' }}>
+          <PlaylistDisplay
+            playlistName={playlist.name}
+            playlistId={playlist.id}
+            playlistImage={user.images[0].url}
+          />
+        </div>
+      )
+    })
+  }
+
   return (
     <div className='home'>
       <BackgroundGradient />
       <div className='home-content'>
         <h1>Good afternoon</h1>
+        <div className='home-playlists'>
+          {renderPlaylists()}
+        </div>
       </div>
     </div>
   )
